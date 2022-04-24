29ccbf803ca202214aedf5c67755dd7e0cba6b1e
Jean Raphael <jr.bordet@gmail.com>
2022-04-24 21:04:01 +0200

    fix

--- Sources/TimeInWords/TimeInWords.swift
+++ Sources/TimeInWords/TimeInWords.swift
@@ -5,14 +5,12 @@ public func timeInWords(h: Int, m: Int) -> String {
     ).timeInWords
 }
 
-public func time12InWords(h: Int, m: Int) -> String {
-    let result = time12(h, minutes: m)
-    
-    // (hour: WordNumber, minutes: WordNumber, accessory: Accessory)
-    
-    let hour = result.hour.shour12
-    let minutes = result.minutes
-    let accessory = result.accessory.word
-    
-    return "\(hour) \(minutes) \(accessory)"
+public func time12InWords(h: Int, m: Int) -> (hour: WordNumber, minutes: WordNumber, accessory: Accessory) {
+    time12(h, minutes: m)
+    //  (hour: WordNumber, minutes: WordNumber, accessory: Accessory)
+//    let hour = result.hour.shour12
+//    let minutes = result.minutes
+//    let accessory = result.accessory.word
+//    
+//    return "\(hour) \(minutes) \(accessory)"
 }
