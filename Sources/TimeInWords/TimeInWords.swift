078c9c6f07f202c03dcea3a1d27db80fdc8ceae3
Jean Raphael <jr.bordet@gmail.com>
2022-04-25 07:52:11 +0200

    Refactor for readability

--- Sources/TimeInWords/TimeInWords.swift
+++ Sources/TimeInWords/TimeInWords.swift
@@ -1,16 +1,16 @@
-public func timeInWords(h: Int, m: Int) -> String {
+public func timeInWords(
+    h: Int,
+    m: Int
+) -> String {
 	Time(
         hour: h,
         minutes: m
     ).timeInWords
 }
 
-public func time12InWords(h: Int, m: Int) -> (hour: WordNumber, minutes: WordNumber, accessory: Accessory) {
+public func time12InWords(
+    h: Int,
+    m: Int
+) -> (hour: WordNumber, minutes: WordNumber, accessory: Accessory) {
     time12(h, minutes: m)
-    //  (hour: WordNumber, minutes: WordNumber, accessory: Accessory)
-//    let hour = result.hour.shour12
-//    let minutes = result.minutes
-//    let accessory = result.accessory.word
-//    
-//    return "\(hour) \(minutes) \(accessory)"
 }
