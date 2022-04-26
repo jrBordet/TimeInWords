dcd45f52d6f43a58faec1c72dc9364279db63adf
Jean Raphael <jr.bordet@gmail.com>
2022-04-26 20:16:30 +0200

    Squashed commit of the following:
    commit 32d580e03522cf742cba90919574044cebe516a1
    Author: Jean Raphael <jr.bordet@gmail.com>
    Date:   Tue Apr 26 20:15:42 2022 +0200
    
        Update TimeInWords.swift
    
    commit c9e3054c73400e0c9907a2ac1f47fae3b2f1f1b5
    Merge: 63d8827 08e1ade
    Author: Jean Raphael <jr.bordet@gmail.com>
    Date:   Tue Apr 26 08:14:12 2022 +0200
    
        Merge branch 'hotfix/0.0.3'

--- Sources/TimeInWords/TimeInWords.swift
+++ Sources/TimeInWords/TimeInWords.swift
@@ -11,6 +11,29 @@ public func timeInWords(
 public func time12InWords(
     h: Int,
     m: Int
-) -> (hour: WordNumber, minutes: WordNumber, accessory: Accessory) {
-    time12(h, minutes: m)
+) -> TimeIn12Components {
+    let result = time12(h, minutes: m)
+    
+    return TimeIn12Components(
+        hour: result.hour,
+        minutes: result.minutes,
+        accessory: result.accessory
+    )    
+}
+
+
+public struct TimeIn12Components: Equatable {
+    public var hour: WordNumber
+    public var minutes: WordNumber
+    public var accessory: Accessory
+    
+    public init (
+     hour: WordNumber,
+     minutes: WordNumber,
+     accessory: Accessory
+    ) {
+        self.hour = hour
+        self.minutes = minutes
+        self.accessory = accessory
+    }
 }
